89330e1c26b14c6dac2058f35552831e6187ad01
josue recoba <josue.huer.reco.22@gamil.com>
2021-11-14 20:00:57 -0600

    Adapter configuracion restante

---
+++ app/src/main/java/com/example/userssp/ItemUserBinding.kt
@@ -0,0 +1,5 @@
+package com.example.userssp
+
+class ItemUserBinding {
+
+}
--- app/src/main/java/com/example/userssp/UserAdapter.kt
+++ app/src/main/java/com/example/userssp/UserAdapter.kt
@@ -1,22 +1,33 @@
 package com.example.userssp
 
+import android.content.Context
+import android.system.Os.bind
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class UserAdapter(private val users: List<User>) : RecyclerView.Adapter<UserAdapter.ViewHolder>(){
 
+    private lateinit var context: Context
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        context = parent.context
+
+        val view = LayoutInflater.from(context).inflate(R.layout.item_user, parent, false)
+
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+       val user = users.get(position)
+       with(holder){
+           binding.tvOrder.text = user.id.toString()
+           binding.tvName.text = user.name
+       }
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = users.size
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
         val binding = ItemUserBinding.bind(view)
